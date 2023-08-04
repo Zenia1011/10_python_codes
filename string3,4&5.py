9b4728cbbb065e22a8dae4b8f4a9667525edcde8
Zenia1011 <141232281+Zenia1011@users.noreply.github.com>
2023-08-04 17:44:54 +0530

    Update string3,4&5.py

--- string3,4&5.py
+++ string3,4&5.py
@@ -1,4 +1,7 @@
-#3, 4 & 5
+#3. Declare a variable named company and assign it to an initial value "Coding For All".
+#4. Print the variable company using *print()*.
+#5. Print the length of the company string using *len()* method and *print().
+
 company = 'Thirty Days Of Python'
 print (company)
 print ("the length of the string is: ",len(company))
