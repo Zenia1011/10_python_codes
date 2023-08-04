57b509ae4618969f4680499b366bcba5577792d0
Zenia1011 <141232281+Zenia1011@users.noreply.github.com>
2023-08-04 17:46:39 +0530

    Update string27.py

--- string27.py
+++ string27.py
@@ -1,4 +1,5 @@
-#slicing because
+#Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
 x = 'You cannot end a sentence with because because because is a conjunction'
 y = x[31:55]
 print (y)
