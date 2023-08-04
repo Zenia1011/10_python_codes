e6d2c8a9e3481385fceb20624dd1f581772bdb94
Zenia1011 <141232281+Zenia1011@users.noreply.github.com>
2023-08-04 17:46:12 +0530

    Update string20.py

--- string20.py
+++ string20.py
@@ -1,3 +1,5 @@
+#Use index to determine the position of the first occurrence of C in Coding For All.
+
 x = 'Coding for all peoplei'
 print ("occurence is: ",x.count('C'))
 print ("occurence is: ",x.count('f'))
